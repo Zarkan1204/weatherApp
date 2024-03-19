e366cd85c1462e9f523e8688cb03003ce5da16ed
Artem Gerasimov <artemtrim@mail.ru>
2024-03-19 10:26:07 +0300

    first commit

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  weatherApp
 //
 //  Created by Мой Macbook on 19.03.2024.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
